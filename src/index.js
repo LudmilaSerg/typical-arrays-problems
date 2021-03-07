c8b23fef50f6e3550ffdaa7c7680a58fcb3d574d
LudmilaSerg <mikoelectrik@gmail.com>
2021-03-07 19:08:37 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == '' || array == [] || array == underfined)
+    return 0;
+  else {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++)
+      if (min > arrai[i])
+        min = arrai[i];
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == '' || array == [] || array == underfined)
+    return 0;
+  else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++)
+      if (max > arrai[i])
+        max = arrai[i];
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == '' || array == [] || array == underfined)
+    return 0;
+  else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++)
+      sum += array[i];
+    return sum / array.length;
+  }
 }
